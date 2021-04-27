2d10ea021837d48e0b72f52a10d243fa19d2e2ca
TriDiamond <bennygx@vip.qq.com>
2021-04-27 12:31:44 +0800

    Fixed fail to load problem.

--- autoload.js
+++ autoload.js
@@ -1,42 +1,43 @@
 // 注意：live2d_path 参数应使用绝对路径
-// const live2d_path = 'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/';
-const live2d_path = './';
+const live2d_path =
+  'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/'
+const local_live2d_path = '/'
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
   return new Promise((resolve, reject) => {
-    let tag;
+    let tag
 
     if (type === 'css') {
-      tag = document.createElement('link');
-      tag.rel = 'stylesheet';
-      tag.href = url;
+      tag = document.createElement('link')
+      tag.rel = 'stylesheet'
+      tag.href = url
     } else if (type === 'js') {
-      tag = document.createElement('script');
-      tag.src = url;
+      tag = document.createElement('script')
+      tag.src = url
     }
     if (tag) {
-      tag.onload = () => resolve(url);
-      tag.onerror = () => reject(url);
-      document.head.appendChild(tag);
+      tag.onload = () => resolve(url)
+      tag.onerror = () => reject(url)
+      document.head.appendChild(tag)
     }
-  });
+  })
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
   Promise.all([
-    loadExternalResource(live2d_path + 'waifu.css', 'css'),
+    loadExternalResource(local_live2d_path + 'waifu.css', 'css'),
     loadExternalResource(live2d_path + 'live2d.min.js', 'js'),
-    loadExternalResource(live2d_path + 'waifu-tips.js', 'js'),
+    loadExternalResource(local_live2d_path + 'waifu-tips.js', 'js'),
   ]).then(() => {
     initWidget({
-      waifuPath: live2d_path + 'waifu-tips.json',
+      waifuPath: local_live2d_path + 'waifu-tips.json',
       apiPath: 'https://live2d.fghrsh.net/api/',
       // cdnPath: 'https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/',
       // apiPath: 'https://api.zsq.im/live2d',
-    });
-  });
+    })
+  })
 }
 // initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
 // API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
@@ -60,4 +61,4 @@ console.log(`
               ﾚ'ヽL__|___i,___,ンﾚ|ノ
                   ﾄ-,/  |___./
                   'ｰ'    !_,.:
-`);
+`)
