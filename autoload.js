7df9c9898318c1f2591546b5c694115d6fb67f08
TriDiamond <bennygx@vip.qq.com>
2021-04-27 12:34:12 +0800

    Fixed path error.

--- autoload.js
+++ autoload.js
@@ -1,7 +1,7 @@
 // 注意：live2d_path 参数应使用绝对路径
 const live2d_path =
   'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/'
-const local_live2d_path = '/'
+const local_live2d_path = './'
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
